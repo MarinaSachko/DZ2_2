554e5b89ac452315bed44d89164c75392262dc19
Marina.Sachko <marina.sachko@mail.ru>
2023-05-04 07:34:17 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        int account;
-        int sumAdd;
-        int prezent;
-        int quantityForPrez;
-        int sumBonus;
-        account = 100;                                         //первоначальный счет
-        prezent = 1;                                           //количество бонусных рублей
-        quantityForPrez = 100;                                 //за какое количетво рублей дается prezent бонусов
-        sumAdd = 1100;                                         //сумма пополнения
+        int account = 100;                                         //первоначальный счет
+        int prezent = 1;                                           //количество бонусных рублей
+        int quantityForPrez = 100;                                 //за какое количетво рублей дается prezent бонусов
+        int sumAdd = 1100;                                         //сумма пополнения
 
-        sumBonus = (sumAdd > 1000) ? sumAdd / 100 * prezent : 0; //сумма бонусных рублей
+        int sumBonus = (sumAdd > 1000) ? sumAdd / 100 * prezent : 0; //сумма бонусных рублей
 
         account = account + sumAdd + sumBonus;
 
